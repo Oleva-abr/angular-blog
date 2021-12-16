f4a7bc6c09324ba1eec769cb1ba21efc0ee7fa1c
Olena <72007221+Oleva-abr@users.noreply.github.com>
2021-12-16 15:59:52 +0200

    create admin components and add routes

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { AdminLayautComponent } from './shared/components/admin-layaut/admin-layaut.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
 
 @NgModule({
-    declarations: [AdminLayautComponent, LoginPageComponent],
+    declarations: [AdminLayautComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
     imports:[
         CommonModule,
         RouterModule.forChild([
             {path:'',component:AdminLayautComponent, children:[
                 {path:'', redirectTo:'/admin/login', pathMatch:'full'},
-                {path: 'login',component:LoginPageComponent}
+                {path: 'login',component:LoginPageComponent},
+                {path: 'dashboard', component:DashboardPageComponent},
+                {path:'create', component: CreatePageComponent},
+                {path:'post/:id/edit', component: EditPageComponent},
             ]}
         ])
     ],
