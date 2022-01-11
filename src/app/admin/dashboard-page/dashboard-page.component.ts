cf00504fe81549a80e5b6da6b712f22147fce4c8
Olena <72007221+Oleva-abr@users.noreply.github.com>
2022-01-11 12:59:56 +0200

    fill dashboard component

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../shared/services/auth.servisre';
+import { PostService } from './../shared/services/post.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Post } from '../shared/interfaces';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts!:Post[]
+  postSub!:Subscription
+  constructor(private postservice: PostService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.postSub=this.postservice.getAllposts().subscribe(posts=>{
+      this.posts=posts
+    })
   }
-test(){
- 
-}
+  remove(id: string){
+
+  }
+  ngOnDestroy() {
+    if(this.postSub){
+      this.postSub.unsubscribe()
+    }
+  }
 }
