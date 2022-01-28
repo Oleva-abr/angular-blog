64f550a1a282ab19cd1887264767c036c8ef07ac
Olena <72007221+Oleva-abr@users.noreply.github.com>
2022-01-28 16:27:24 +0200

    add edit page

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -2,34 +2,59 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Post } from './../shared/interfaces';
 import { PostService } from './../shared/services/post.service';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { switchMap } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
-  form!: FormGroup
+export class EditPageComponent implements OnInit, OnDestroy {
+  form!: FormGroup;
+  post!: Post;
+  submitted=false;
+
+  uSub: Subscription | undefined
+
   constructor(
-    private route : ActivatedRoute,
+    private route: ActivatedRoute,
     private postService: PostService,
   ) { }
 
+
+
   ngOnInit(): void {
     this.route.params
-    .pipe(switchMap((params : Params)=>{
-      return this.postService.getById(params['id'])
-    })).subscribe((post: Post)=>{
-      this.form = new FormGroup({
-        title: new FormControl(post.title, Validators.required),
-        text: new FormControl(post.text, Validators.required),
+      .pipe(switchMap((params: Params) => {
+        return this.postService.getById(params['id'])
+      })).subscribe((post: Post) => {
+        this.post = post
+        this.form = new FormGroup({
+          title: new FormControl(post.title, Validators.required),
+          text: new FormControl(post.text, Validators.required),
+        })
       })
-    })
   }
+  ngOnDestroy() {
+    if (this.uSub) {
+      this.uSub.unsubscribe()
+    }
+  }
+  
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
 
-  submit(){
-    
+    this.submitted = true
+
+    this.uSub = this.postService.update({
+      ...this.post,
+      text: this.form.value.text,
+      title: this.form.value.title
+    }).subscribe(() => {
+      this.submitted = false
+    })
   }
 }
